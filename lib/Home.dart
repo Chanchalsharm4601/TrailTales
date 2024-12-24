4af61761492cbd9f9ea128d660d29f9817f22cac
chanchal <adarsingh2002@gmail.com>
2024-12-25 00:01:13 +0530

    new commit

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:trail_tales/CreateAccountScreen.dart';
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -7,8 +9,122 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool _isPasswordHidden = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Image.asset(
+                "asset/background.jpg",
+                fit: BoxFit.cover,
+                height: screenHeight,
+                width: screenWidth,
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                    screenWidth * 0.1, screenHeight * 0.3, screenWidth * 0.1, screenHeight * 0.3),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextField(
+                      style: TextStyle(color: Colors.white),
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.email_outlined, color: Colors.white),
+                        labelText: "Enter your Email",
+                        labelStyle: TextStyle(color: Colors.white),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+                    TextField(
+                      style: TextStyle(color: Colors.white),
+                      obscureText: _isPasswordHidden,
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock, color: Colors.white),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isPasswordHidden ? Icons.visibility : Icons.visibility_off,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _isPasswordHidden = !_isPasswordHidden;
+                            });
+                          },
+                        ),
+                        labelText: "Enter Password",
+                        labelStyle: TextStyle(color: Colors.white),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.04),
+                    Center(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          shadowColor: Colors.transparent,
+                          side: BorderSide(color: Colors.white, width: 1),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "Sign In",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              Navigator.push(context, MaterialPageRoute(builder: (context) => CreateAccount() ));
+                            });
+                          },
+                          child: Text(
+                            "Create Account",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                        ),
+                        Text("|", style: TextStyle(color: Colors.white, fontSize: 16)),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Forget Password",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
